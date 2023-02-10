4b26e99cca025e54cadb694809e7a143fd450f9d
Doug Brockel <workmail.jsph@gmail.com>
2023-02-10 15:12:37 -0600

    CAPSTONE WORK, EFFECTS, CSS, FLEXBOX

---
+++ index.js
@@ -0,0 +1,8 @@
+let home = document.querySelector("#Home");
+let about = document.querySelector("#About");
+let contact = document.querySelector("#Contact");
+let create = document.querySelector("#Create");
+
+
+
+
