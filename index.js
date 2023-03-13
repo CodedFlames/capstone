fb1956413146d5046b6b333cc0a48a6367a15434
Doug Brockel <workmail.jsph@gmail.com>
2023-03-12 20:36:58 -0500

    minor changes finalizing

--- index.js
+++ index.js
@@ -8,53 +8,17 @@ import axios from "axios";
 const router = new Navigo("/");
 
 function render(state = store.Home) {
-  if (state["view"] === "Home") {
-    axios.get(process.env.NASAURLKEY).then(Res => {
-      store["Home"].photoTitle = Res.data.title;
-      store["Home"].photoUrl = Res.data.hdurl;
-      store["Home"].photoText = Res.data.explanation;
-    });
-  }
   document.querySelector("#meta").innerHTML += `${Head(state)}`;
   document.querySelector("#root").innerHTML = `
   ${Nav(store.Links)}
   ${Main(state)}`;
-
   afterRender(state);
   router.updatePageLinks();
 }
 
-router.hooks({
-  before: (done, params) => {
-    const view =
-      params && params.hasOwnProperty("page")
-        ? capitalize(params.page)
-        : "Home";
-    // Add a switch case statement to handle multiple routes
-    switch (view) {
-      case "/":
-      case "Home":
-        //axios
-
-        //axios
-        done(); //tell navigo im done here.
-        break;
-      default:
-        done();
-    }
-  },
-  already: params => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
-    render(store[view]);
-  }
-});
-
 function afterRender(state) {
   // NAVBARS
-  console.log("In afterRender.");
+  // console.log("In afterRender."); //debug
   let barr = document.querySelector("#navBars");
   let barrOpen = false;
   barr.addEventListener("click", () => {
@@ -74,6 +38,38 @@ function afterRender(state) {
       break;
   }
 }
+
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home":
+        //axios
+        axios.get(process.env.NASAURLKEY).then(Res => {
+          console.log("Tc's left;", Res.headers["x-ratelimit-remaining"]);
+          store["Home"].photoTitle = Res.data.title;
+          store["Home"].photoUrl = Res.data.hdurl;
+          store["Home"].photoText = Res.data.explanation;
+        });
+        //axios
+        done(); //tell navigo im done here.
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    render(store[view]);
+  }
+});
 
 router
   .on({
--- store/Home.js
+++ store/Home.js
@@ -2,6 +2,7 @@ export default {
   title: "Lock-It",
   topper: "Open a file.",
   view: "Home",
+  totalkeys: 0,
   photoTitle: "",
   photoUrl: "",
   photoText: ""
